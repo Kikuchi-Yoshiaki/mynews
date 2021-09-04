db5836fd5084344ced8fdc9613fbee925246aa61
Kikuchi-Yoshiaki <kikuyoshi@wind.ocn.ne.jp>
2021-09-04 12:50:54 +0000

    カリキュラム19課題+18修正

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device, inital-scale=1">
+        <title>MyProfile</title>
+    </head>
+    <body>
+        <h1>プロフィール作成画面</h1>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, inital-scale=1">
+        <title>EditProfile</title>
+    </head>
+    <body>
+        <h1>プロフィール選択画面</h1>
+    </body>
+</html>
